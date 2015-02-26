4d3e569f5bae08120cfdda057b3b78e36eb98277
okscounter <akorach@hotmail.com>
2015-02-26 13:50:11 +0100

    Random in decideNextMove, corrected initialise game and insertCoin

--- ISP_GameProject/src/GameLogic.java
+++ ISP_GameProject/src/GameLogic.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 
 public class GameLogic implements IGameLogic {
     private int x = 0;
@@ -15,7 +17,7 @@ public class GameLogic implements IGameLogic {
         this.playerID = playerID;
         board = new int[x][y];
         for(int i = 0; i < x; i++){
-			for(int n = 0; n < x; n++){
+			for(int n = 0; n < y; n++){
 				board[i][n] = 0;
 			}
     	}
@@ -42,11 +44,11 @@ public class GameLogic implements IGameLogic {
 
 
     public void insertCoin(int column, int playerID) {
-        for(int i = x - 1 ; i >= 0 ; i--)
+        for(int i = y - 1 ; i >= 0 ; i--)
         {
-        	if(board[i][column] == 0)
+        	if(board[column][i] == 0)
         	{
-        		board[i][column] = playerID;
+        		board[column][i] = playerID;
         		break;
         	}
         		
@@ -54,8 +56,14 @@ public class GameLogic implements IGameLogic {
     }
 
     public int decideNextMove() {
-        //TODO Write your implementation for this method
-        return 0;
+        Random rand = new Random();
+        int r;
+        
+        do
+        	r = rand.nextInt(x);
+        while(board[r][0] != 0);
+        
+        return r;
     }
 
 }
