d5236a73479de6b54d381fe8e5ecc7947efeeb04
AaronGitH <agor@itu.de>
2015-03-01 13:06:13 +0100

    gameFinished() worked

--- ISP_GameProject/src/GameLogic.java
+++ ISP_GameProject/src/GameLogic.java
@@ -1,11 +1,12 @@
 
 public class GameLogic implements IGameLogic {
-    private int x = 0;
-    private int y = 0;
+    private int x = 0;  // counting horizontal, starting from left
+    private int y = 0;  // counting vertical, starting at bottom
     private int playerID;
     private int[][] board;
     
     public GameLogic() {
+    	
         //TODO Write your implementation for this method
     }
 	
@@ -15,47 +16,141 @@ public class GameLogic implements IGameLogic {
         this.playerID = playerID;
         board = new int[x][y];
         for(int i = 0; i < x; i++){
-			for(int n = 0; n < x; n++){
+			for(int n = 0; n < y; n++){
 				board[i][n] = 0;
 			}
     	}
-        //TODO Write your implementation for this method
     }
-	
-    private boolean checkRow(){
-    	for(int i = 0; i < x; i++){
+    
+    
+    private boolean checkAllDirections(int playerID){
+    	int inARowCount = 0;
+    	
+    	// horizontal check
+    	for(int i = 0; i < y; i++){
 			for(int n = 0; n < x; n++){
-				//if(board[i][n] == );
+				if(board[n][i] == playerID){
+					inARowCount++;
+					if(inARowCount >= 4){ return true; }
+				}
+				else{ inARowCount = 0; }
 			}
-			
+			inARowCount = 0;
+    	}
+    	
+    	// vertical check
+    	for(int i = 0; i < x; i++){
+			for(int n = 0; n < y; n++){
+				if(board[i][n] == playerID){
+					inARowCount++;
+					if(inARowCount >= 4){ return true; }
+				}
+				else{ inARowCount = 0; }
+			}
+			inARowCount = 0;
+    	}
+    	
+    	
+    	// diagonal in this / direction
+    	int diagonals = x + y - 1;
+    	int fieldsInDiagonal = -1;
+    	for(int i = 0; i < diagonals; i++){
+    		if( i < x ){
+    			fieldsInDiagonal = 0;
+    			while(i + fieldsInDiagonal < x && 0 + fieldsInDiagonal < y){ //###
+        			fieldsInDiagonal++;
+        		}
+    			for(int n = 0; n < fieldsInDiagonal; n++){
+    				if(board[i + n][0 + n] == playerID){ //###
+    					inARowCount++;
+    					if(inARowCount >= 4){ return true; }
+    				}
+    				else{ inARowCount = 0; }	
+    			}
+    			inARowCount = 0;
+    		}
+    		else {
+    			fieldsInDiagonal = 0;
+    			while(0 + fieldsInDiagonal < x && i - x + fieldsInDiagonal < y){ //###
+        			fieldsInDiagonal++;
+        		}
+    			for(int n = 0; n < fieldsInDiagonal; n++){
+    				if(board[0 + n][i - x + n] == playerID){ //###
+    					inARowCount++;
+    					if(inARowCount >= 4){ return true; }
+    				}
+    				else{ inARowCount = 0; }	
+    			}
+    			inARowCount = 0;
+    		}
+    	}
+    	
+    	// diagonal in this \ direction
+    	for(int i = diagonals -1; i >= 0; i--){
+    		if( i < x ){
+    			fieldsInDiagonal = 0;
+    			while(i - fieldsInDiagonal >= 0 && 0 + fieldsInDiagonal < y){ //###
+        			fieldsInDiagonal++;
+        		}
+    			for(int n = 0; n < fieldsInDiagonal; n++){
+    				if(board[i - n][0 + n] == playerID){ //###
+    					inARowCount++;
+    					if(inARowCount >= 4){ return true; }
+    				}
+    				else{ inARowCount = 0; }	
+    			}
+    			inARowCount = 0;
+    		}
+    		else {
+    			fieldsInDiagonal = 0;
+    			while(x - 1 - fieldsInDiagonal >= 0 && i + x + fieldsInDiagonal < y){ //###
+        			fieldsInDiagonal++;
+        		}
+    			for(int n = 0; n < fieldsInDiagonal; n++){
+    				if(board[x - 1 - n][i - x + n] == playerID){ //###
+    					inARowCount++;
+    					if(inARowCount >= 4){ return true; }
+    				}
+    				else{ inARowCount = 0; }	
+    			}
+    			inARowCount = 0;
+    		}
     	}
     	return false;
+    }
+    
+    private boolean boardIsFull(){
+    	boolean isFull = true;
+    	for(int i = 0; i < x; i++){
+    		if(board[i][y-1] == 0){ isFull = false; }
+    	}
+    	return isFull;
     }
     
     
     public Winner gameFinished() {
     	//TODO Write your implementation for this method
-    	checkRow();
-        
-        return Winner.NOT_FINISHED;
+    	if(checkAllDirections(1)){ System.out.println("YEPPPI"); return Winner.PLAYER1;}
+    	if(checkAllDirections(2)){ System.out.println("HURRA!"); return Winner.PLAYER2;}
+    	
+    	if(boardIsFull()){ System.out.println("Tie!"); return Winner.TIE; }
+    	else{ System.out.println("Next round..."); return Winner.NOT_FINISHED; }
     }
-
+    
 
     public void insertCoin(int column, int playerID) {
-        for(int i = x - 1 ; i >= 0 ; i--)
-        {
-        	if(board[i][column] == 0)
-        	{
-        		board[i][column] = playerID;
+        for(int i = 0; i < y; i++){
+        	if(board[column][i] == 0){
+        		board[column][i] = playerID;
         		break;
         	}
-        		
         }
     }
 
+    
     public int decideNextMove() {
         //TODO Write your implementation for this method
-        return 0;
+    	int i = (int) (Math.random() * (x)); System.out.println(i); return i;
+    	//return 0;
     }
-
 }
